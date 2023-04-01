f6e1184336fb239d72c64de5f7153b69ef3fd929
nshan651 <nickesg44@gmail.com>
2023-04-01 14:44:24 -0500

    Refactored and updated docs

--- .ccls-cache/@home@nick@git@comp339/bloom@src@bloom.cc
+++ .ccls-cache/@home@nick@git@comp339/bloom@src@bloom.cc
@@ -1,3 +1,8 @@
+/**
+ * @file bloom.cc
+ * @brief Implementation of a bloom filter in C++.
+ */
+
 #include <bloom.hh>
 #include <hash.hh>
 #include <CLI/CLI.hpp>
@@ -6,32 +11,37 @@ using namespace std;
 
 class BloomFilter 
 {
-    int m, k, n;
-    vector<lamb_hash> hash_set;
-    int *data;
-    int collisions;
-
 public:
+    int m, k, n;
+    int *data;
+    vector<lamb_hash> hash_set;
+    int collisions;
+    vector<string> collision_words;
+
     BloomFilter(const int m, const int k, const vector<lamb_hash> hash_set) 
-    /** Bloom filter constructor.
-        Arguments:
-            m | the number of bits
-            k | the number of hash functions
-            hash_set | The set of hash functions to use
-    */
+    /**
+     * @brief Bloom Filter constructor. 
+     * @param m number of bits
+     * @param k number of hash functions
+     * @param hash_set set of all hash functions
+     */
     {
         this->m = m;
         this->k = k;
         this->n = 0;
         this->hash_set = hash_set;
         this->collisions = 0;
+        this->collision_words;
 
         this->data = new int[m];
         memset(this->data, 0, m*sizeof(int));
     }
 
     void insert(const string& element)
-    /** Insert hash into Bloom Filter. */
+    /** 
+     * @brief Insert hash into Bloom Filter. 
+     * @param element is the word to insert
+     */
     {
         bool collision = true;
         for (auto fun : hash_set) {
@@ -40,45 +50,26 @@ public:
             if (data[hash] == 0) collision = false;
             data[hash] = 1;
         }
-        if (collision == true) collisions++;
+        if (collision == true) {
+            collision_words.push_back(element);
+            collisions++;
+        }
         n++;
     }
 
     double search(const string& element)
-    /** Search for a hash in the Bloom Filter. */
+    /** 
+     * @brief Search for a hash in the Bloom Filter.
+     * @param element is the word to search
+     * @return The false probability
+     */
     {
         for (auto fun : hash_set) {
             size_t hash = fun(element, m) % m;
-            if (data[hash] == 0) {
-                /* cout << "Not in Bloom Filter\n"; */
-                return -1.0;
-            }
+            if (data[hash] == 0) return -1.0;
         }
         double prob = pow(1.0 - pow(1.0 - 1.0/m, k*n), k);
-        /* cout << "Might be in Bloom Filter with false positive probability " << to_string(prob) << "\n"; */
         return prob;
-    }
-
-    /** Accessors */
-    int *get_data()
-    {
-        return data;
-    }
-    int get_m()
-    {
-        return m;
-    }
-    int get_n()
-    {
-        return n;
-    }
-    int get_k()
-    {
-        return k;
-    }
-    int get_collisions()
-    {
-        return collisions;
     }
 
     ~BloomFilter() 
@@ -88,13 +79,18 @@ public:
     }
 };
 
-int init(const int m, 
-         const int k, 
-         const vector<lamb_hash> hash_set, 
-         const string& input, 
-         const vector<string> &keywords)
-/** Initialize the Bloom Filter*/
+int bloom(const int m, const int k, const string& search_query, const string& input)
+/** 
+ * @brief Initializes a Bloom Filter
+ * @param m is the number of bits 
+ * @param k is the number of hash functions
+ * @param search_query is the comma-separated string of words to search
+ * @param input is the input stream
+ * @returns The number of collisions
+ */
 {
+    vector<lamb_hash> hash_set = get_hashset(k);
+
     /* Put bf on the heap */
     BloomFilter* bf = new BloomFilter(m, k, hash_set);
 
@@ -104,51 +100,57 @@ int init(const int m,
     while (infile >> word)
         bf->insert(word);
     infile.close();
-    
-    /* set<double> uniques(bf->get_data().begin(), bf->get_data().end()); */
-    /* int collisions = sizeof(bf->get_data()) - uniques.size(); */
-    spdlog::info("Collisions: {}", bf->get_collisions());
 
-    /* Search all keywords */
-    for (auto& keyword : keywords) 
-    spdlog::debug("{} ", bf->search(keyword));
+    /* Log the number of collisions */
+    spdlog::info("Collisions: {}", bf->collisions);
+
+    /* If there are searches, get the false positive probability for each query */
+    if (!search_query.empty()) {
+        stringstream ss(search_query);
+        ostringstream f_pos;
+        string keyword;
+        while (getline(ss, keyword, ','))
+            f_pos << bf->search(keyword) << " ";
+
+        spdlog::info("False positive probabilities: {}", f_pos.str());
+    }
+
+    ostringstream collision_inf;
+    for (auto cw : bf->collision_words)
+            collision_inf << cw << " ";
+
+    spdlog::debug("Collision words: {}", collision_inf.str());
 
     /* Deallocate when finished */
+    int collisions = bf->collisions;
     delete bf; 
 
-    return 0;
+    return collisions;
 }
 
-
+#ifdef BLOOM
 int main(int argc, char **argv) 
 {
 
     spdlog::set_level(spdlog::level::debug);
+    /* spdlog::set_level(spdlog::level::info); */
 
-    int m = 0;
-    int k = 0;
+    int m;
+    int k;
     string input; 
     string search_query;
-    vector<string> keywords;
 
     CLI::App app("BloomFilter CLI");
     app.add_option("-m", m, "Number of bits")->default_val(100);
     app.add_option("-k", k, "Number of hash functions")->default_val(2);
     app.add_option("-i", input, "Input method")->default_val("./data/american-english");
-    app.add_option("-s", search_query, "Comma-separated list of search terms")->default_val("Hello,No Way,ge");
+    app.add_option("-s", search_query, "Comma-separated list of search terms")->default_val("");
     // TODO: Add option for list of hash functions
     CLI11_PARSE(app, argc, argv);
 
-    vector<lamb_hash> hash_set = get_hashset(k);
-
-    stringstream ss(search_query);
-    string keyword;
-    while (getline(ss, keyword, ','))
-        keywords.push_back(keyword);
-
-
     /* Init bloom filter */
-    init(m, k, hash_set, input, keywords);
+    bloom(m, k, search_query, input);
 
     return 0;
 }
+#endif
